fb0d5c455ea38621f173ad209ea089422c68aac2
mike <m.lenskyi@litres.ru>
2020-09-18 17:28:44 +0300

    circle radius

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+const pi = 3.1415
+
+// func main() {
+//     var phrase string = "Go рулит!"
+//     fmt.Println(phrase)
+// }
 func main() {
-    fmt.Println("Go рулит!")
+	printCircleArea()
+}
+
+func printCircleArea() {
+	circleRadius := 2
+	circleArea := math.Pow(float64(circleRadius), 2) * float64(pi)
+
+	fmt.Printf("Радиус: %d см.\n", circleRadius)
+	fmt.Printf("Площадь круга: %f32 см. кв.\n", circleArea)
 }
