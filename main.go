42bb712d0fb93bf99e528b6a885dca257753330a
mike <m.lenskyi@litres.ru>
2020-09-21 17:51:14 +0300

    if usage

--- main.go
+++ main.go
@@ -3,22 +3,36 @@ package main
 import (
 	"fmt"
 	"math"
+	"errors"
 )
 
-const pi = 3.1415
+// const pi = 3.1415
+// const pi = math.Pi
 
 // func main() {
 //     var phrase string = "Go рулит!"
 //     fmt.Println(phrase)
 // }
 func main() {
-	printCircleArea()
+	printCircleArea(2)
+	printCircleArea(4)
+	printCircleArea(-2)
 }
 
-func printCircleArea() {
-	circleRadius := 2
-	circleArea := math.Pow(float64(circleRadius), 2) * float64(pi)
+func printCircleArea(radius int) {
+	circlArea, err := calcCircleArea(radius)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-	fmt.Printf("Радиус: %d см.\n", circleRadius)
-	fmt.Printf("Площадь круга: %f32 см. кв.\n", circleArea)
+	fmt.Printf("Радиус: %d см.\n", radius)
+	fmt.Printf("Площадь круга: %f32 см. кв.\n", circlArea)
+}
+
+func calcCircleArea(radius int) (float64, error) {
+	if radius <= 0 {
+		return float64(0), errors.New("Радиус не может быть отрицательным!")
+	}
+	return math.Pow(float64(radius), 2) * float64(math.Pi), nil
 }
